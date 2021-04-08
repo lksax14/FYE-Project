feb9a56ab931303828e99b659c01986d1abd9292
lksax14 <82056191+lksax14@users.noreply.github.com>
2021-04-08 13:51:14 -0400

    Add files via upload

---
+++ project_function_lsachse.m
@@ -0,0 +1,53 @@
+function [v_naught] = M2_Algorithm_014_24(data)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% ENGR 132
+% Program Description
+% The purpose of this function is to determine the initial velocities of
+% each concentration of the different substrates.
+%
+% Function Call
+% M2_Algorithm_014_24(data)
+%
+% Input Arguments
+% data - The data that contains the information needed on the different
+% substrates
+%
+% Output Arguments
+% v_naught - The array that will be returned with each initial velocity for
+% each concentration of substrate (m/s)
+%
+% Assignment Information
+%   Assignment:     M02, Problem 1
+%   Team member:    William Albright, albrighw@purdue.edu [repeat for each person]
+%                   Leo Sakuma, lsakuma@purdue.edu
+%                   Luke Sachse, lsachse@purdue.edu
+%                   Alec Liu, ajliu@purdue.edu
+%   Team ID:        014-24
+%   Academic Integrity:
+%     [] We worked with one or more peers but our collaboration
+%        maintained academic integrity.
+%     Peers we worked with: N/A
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% ____________________
+%% INITIALIZATION
+data = readmatrix('Data_nextGen_KEtesting_allresults.csv');
+
+%% ____________________
+%% CALCULATIONS
+
+
+%% ____________________
+%% FORMATTED TEXT/FIGURE DISPLAYS
+
+
+%% ____________________
+%% RESULTS
+
+
+%% ____________________
+%% ACADEMIC INTEGRITY STATEMENT
+% We have not used source code obtained from any other unauthorized
+% source, either modified or unmodified. Neither have we provided
+% access to my code to another. The program we are submitting
+% is our own original work.
