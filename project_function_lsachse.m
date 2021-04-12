6aa8321189dae0c32c6e66731ff0d8b43b6b7331
williamalbright616 <82056312+williamalbright616@users.noreply.github.com>
2021-04-12 18:41:21 -0400

    Update project_function_lsachse.m

--- project_function_lsachse.m
+++ project_function_lsachse.m
@@ -31,12 +31,57 @@ function [v_naught] = M2_Algorithm_014_24(data)
 
 %% ____________________
 %% INITIALIZATION
-data = readmatrix('Data_nextGen_KEtesting_allresults.csv');
+data = readmatrix('Data_nextGen_KEtesting_allresults.csv'); %Imports the file to be used
+data_x = data(3:end, 1); %Time data (seconds)
+vnaught = zeros([1, 50]); %Pre-allocates the array of vnaughts
+z = 1; %Vnaught index counter
+col = 2; %Data column index counter
 
-%% ____________________
+
+%%____________________
 %% CALCULATIONS
+%These statements will fill an array of vnaughts respective to their
+%concentrations. A simple moving average (SMA) is used to try and smooth
+%the noisy data as much as possible. An average of the test data and the
+%replicate test data is then taken and the initial slope of that line
+%(calculated within the first 21 seconds) and that value is placed into the
+%vnaught array. This is repeated for each concentration to add up to a
+%total of 50 vnaughts.
+for ct = 1:50
+    j = 1; %SMA index counter
+    data_y = data(3:end, col); %Test data ([P] (uM))
+    data_yrep = data(3:end, col + 10); %Replicate test data ([P] (uM))
+    SMA = zeros([1, 7482]); %Pre-allocates an array of zeros for use
+    SMAyrep = zeros([1, 7482]); %Pre-allocates an array of zeros for use
+    col = col + 1;
+    for i = 3:numel(data_y)
+        for k = 1:-1:0
+            SMA(j) = SMA(j) + (data_y(i - k));
+            SMAyrep(j) = SMAyrep(j) + (data_yrep(i - k));
+        end
+        SMA(j) = SMA(j) / 2;
+        SMAyrep(j) = SMAyrep(j) / 2;
+        j = j + 1;
+    end
+    data_SMAave = (SMA(1:end) + SMAyrep(1:end)) / 2;
+    coeffs = polyfit(data_x(11:21), data_SMAave(1:11), 1);
+    vnaught(z) = coeffs(1);
+    z = z + 1;
+    if col == 11
+        col = 21;
+    end
+    if col == 31
+        col = 41;
+    end
+    if col == 51
+        col = 61;
+    end
+    if col == 71
+        col = 81;
+    end
+end
 
-
+v_naught = vnaught;
 %% ____________________
 %% FORMATTED TEXT/FIGURE DISPLAYS
 
