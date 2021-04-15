fabeb7cb99f30a09bdc7e8085a86c29adb8bdd77
ajliu02 <ajliu@purdue.edu>
2021-04-15 15:11:06 -0400

    Edits from 4/15/21 in Class

--- project_function_lsachse.m
+++ project_function_lsachse.m
@@ -28,31 +28,35 @@ function [v_naught] = M2_Algorithm_014_24(data)
 %        maintained academic integrity.
 %     Peers we worked with: N/A
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
+%Vi=Vmax−Km×Vi/[S] %this is the equation we need to use.
+% "Vmax is the highest vnaught value of the ones you calculated using different concentrations"
+% Vmax might be the last value in each row?
 %% ____________________
 %% INITIALIZATION
 data = readmatrix('Data_nextGen_KEtesting_allresults.csv'); %Imports the file to be used
 data_x = data(3:end, 1); %Time data (seconds)
-vnaught = zeros([1, 50]); %Pre-allocates the array of vnaughts
-z = 1; %Vnaught index counter
+vnaught = zeros([5, 10]); %Pre-allocates the array of vnaughts
+Vrow = 1; %Vnaught row counter
+Vcol = 1; %Vnaught column counter
 col = 2; %Data column index counter
+vMax =  zeros([5, 1]); % Pre-allocates an array of vmaxes
 
 
 %%____________________
 %% CALCULATIONS
-% These statements will fill an array of vnaughts respective to their
-% concentrations. A simple moving average (SMA) is used to try and smooth
-% the noisy data as much as possible. An average of the test data and the
-% replicate test data is then taken and the initial slope of that line
-% (calculated within the first 21 seconds) and that value is placed into the
-% vnaught array. This is repeated for each concentration to add up to a
-% total of 50 vnaughts.
+%These statements will fill an array of vnaughts respective to their
+%concentrations. A simple moving average (SMA) is used to try and smooth
+%the noisy data as much as possible. An average of the test data and the
+%replicate test data is then taken and the initial slope of that line
+%(calculated within the first 20 seconds) and that value is placed into the
+%vnaught array. This is repeated for each concentration to add up to a
+%total of 50 vnaughts.
 for ct = 1:50
     j = 1; %SMA index counter
     data_y = data(3:end, col); %Test data ([P] (uM))
     data_yrep = data(3:end, col + 10); %Replicate test data ([P] (uM))
-    SMA = zeros([1, 7482]); %Pre-allocates an array of zeros for use. This array represents the Simple Moving Average values
-    SMAyrep = zeros([1, 7482]); %Pre-allocates an array of zeros for use. This array represents the Simple Moving Average values for the replicate test data
+    SMA = zeros([1, 7482]); %Pre-allocates an array of zeros for use
+    SMAyrep = zeros([1, 7482]); %Pre-allocates an array of zeros for use
     col = col + 1;
     for i = 3:numel(data_y)
         for k = 1:-1:0
@@ -63,11 +67,14 @@ for ct = 1:50
         SMAyrep(j) = SMAyrep(j) / 2;
         j = j + 1;
     end
-    data_SMAave = (SMA(1:end) + SMAyrep(1:end)) / 2; %This is the average of the SMA and SMArep values to find the best fitting and smoothed line that represents the data accurately
-    coeffs = polyfit(data_x(11:21), data_SMAave(1:11), 1);
-    vnaught(z) = coeffs(1); %This is Vnaught, the initial slope of the line, and will be placed into the array that is returned
-    z = z + 1;
-    % If statement to pass over test duplicates
+    data_SMAave = (SMA(1:end) + SMAyrep(1:end)) / 2;
+    coeffs = polyfit(data_x(2:20), data_SMAave(1:19), 1);
+    vnaught(Vrow, Vcol) = coeffs(1);
+    Vcol = Vcol + 1;
+    if Vcol == 11
+        Vcol = 1;
+        Vrow = Vrow + 1;
+    end
     if col == 11
         col = 21;
     end
@@ -83,6 +90,10 @@ for ct = 1:50
 end
 
 v_naught = vnaught;
+
+vMax = max(v_naught, [], 2);
+
+
 %% ____________________
 %% FORMATTED TEXT/FIGURE DISPLAYS
 
